07cccc629fbb673fdef1481c13806c416e27a70e
kushagragoyal18 <9523kushagragoyal@gmail.com>
2026-02-14 22:13:48 +0530

    Refactor function to find minimum chocolate difference

--- Ques3.1.py
+++ Ques3.1.py
@@ -1,21 +1,19 @@
 #Given an array arr[] of positive integers, where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are m students, the task is to distribute chocolate packets among m students such that - i. Each student gets exactly one packet. ii. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is minimum and return that minimum possible difference.
 class Solution:
-    def smallestSubWithSum(self, x, arr):
+    def findMinDiff(self, arr, m):
         n = len(arr)
-        min_len = float('inf')
-        curr_sum = 0
-        start = 0
-
-        for end in range(n):
-            curr_sum += arr[end]
-
-            # Shrink window while sum > x
-            while curr_sum > x:
-                min_len = min(min_len, end - start + 1)
-                curr_sum -= arr[start]
-                start += 1
-
-        if min_len == float('inf'):
+        
+        if m == 0 or n == 0:
             return 0
-
-        return min_len
+        
+        if m > n:
+            return -1
+        
+        arr.sort()
+        
+        min_diff = float('inf')
+        
+        for i in range(n - m + 1):
+            min_diff = min(min_diff, arr[i + m - 1] - arr[i])
+        
+        return min_diff
